c9f8d9e27f2db7ce2d8f60a266c69513b0a22267
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2024-02-20 12:01:04 +0100

    feat: amélioration de l'accessibilité du composant CustomTable.

--- hr-net/src/component/CustomTable/index.js
+++ hr-net/src/component/CustomTable/index.js
@@ -139,8 +139,20 @@ function CustomTable({ title, columns, data }) {
                                 <div className={style.columnNameContainer}>
                                     {name}
                                     <div className={style.arrowsContainer}>
-                                        <button className={style.arrowUp} onClick={() => handleSort(key, true)}><div></div></button>
-                                        <button className={style.arrowDown} onClick={() => handleSort(key, false)}><div></div></button>
+                                        <div className={`${style.arrow} ${style.arrowUp}`}>
+                                            <input
+                                                type="button"
+                                                aria-label="sort in ascending order"
+                                                onClick={() => handleSort(key, true)}
+                                            />
+                                        </div>
+                                        <div className={`${style.arrow} ${style.arrowDown}`}>
+                                            <input
+                                                type="button"
+                                                aria-label="sort in descending order"
+                                                onClick={() => handleSort(key, false)}
+                                            />
+                                        </div>
                                     </div>
                                 </div>
                             </th>
